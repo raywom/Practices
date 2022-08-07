ca5e5fc7c2324d0baf4279fddd88552d74a05ee4
@raywom <40425058+raywom@users.noreply.github.com>
2022-08-08 00:21:17 +0200

    fix

---
+++ Array/RemoveDuplicate.cs
@@ -0,0 +1,27 @@
+﻿namespace Array;
+
+public class RemoveDuplicate
+{
+    public static int RemoveDuplicates(int[] nums) {
+        var size = nums.Length;
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = i + 1; j < size; j++)
+            {
+                if (nums[i] == nums[j])
+                {
+                    for(int k = j; k < size - 1; k++)
+                    {
+                        nums[k] = nums[k + 1];
+                    }
+
+                    j--;
+                    size--;
+                }
+            }
+        }
+
+        var arr = nums[0..size];
+        return arr.Length+1;
+    }
+}
