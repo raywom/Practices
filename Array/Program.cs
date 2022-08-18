7efc80a7c8abbabd8b017ba6c85080a2323395cf
@raywom <40425058+raywom@users.noreply.github.com>
2022-08-18 22:30:09 +0200

    add

---
+++ Array/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Solution solution = new Solution();
+int[] nums = new int[] {1, 2, 3, 4};
+var flex = solution.RunningSum(nums);
+
+public class Solution {
+    public int[] RunningSum(int[] nums) {
+        for (int i = 0; i < nums.Length; i++) {
+            if (i != 0)
+            {
+                nums[i] = nums[i - 1] + nums[i];
+            }
+        }
+        return nums;
+    }
+}
---
+++ RunningSumOf1dArray/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Hello, World!");
+
+public class Solution {
+    public int[] RunningSum(int[] nums) {
+        for (int i = 0; i < nums.Length; i++) {
+            if (i != 0)
+            {
+                nums[i] = nums[i - 1] + nums[i];
+            }
+        }
+        return nums;
+    }
+}
