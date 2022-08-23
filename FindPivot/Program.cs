57cca063f0cb152645647ac688442d5331b7f655
@raywom <40425058+raywom@users.noreply.github.com>
2022-08-23 23:26:59 +0200

    add findpivot and isomorphic strings

---
+++ FindPivot/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+int[] nums = {1,7,3,6,5,6};
+Solution solution = new Solution();
+int flex = solution.PivotIndex(nums);
+Console.WriteLine(flex);
+
+public class Solution {
+    public int PivotIndex(int[] nums) {
+        int sum = 0;
+        for (int i = 0; i < nums.Length; i++) {
+            sum += nums[i];
+        }
+        int leftSum = 0;
+        for (int i = 0; i < nums.Length; i++) {
+            if (leftSum == sum - leftSum - nums[i]) {
+                return i;
+            }
+            leftSum += nums[i];
+        }
+        return -1;
+    }
+}
---
+++ IsomorphicStrings/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+public class Solution {
+    public bool IsIsomorphic(string s, string t) {
+        Dictionary<char, char> map = new Dictionary<char, char>();
+        Dictionary<char, char> map2 = new Dictionary<char, char>();
+        for (int i = 0; i < s.Length; i++) {
+            if (!map.ContainsKey(s[i])) {
+                map.Add(s[i], t[i]);
+            }
+            if (!map2.ContainsKey(t[i])) {
+                map2.Add(t[i], s[i]);
+            }
+            if (map[s[i]] != t[i] || map2[t[i]] != s[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
