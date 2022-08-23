72dee1423b9939388775f3c04f572a2a48f659ac
@raywom <40425058+raywom@users.noreply.github.com>
2022-08-23 23:58:39 +0200

    another one

---
+++ IsSubsequence/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string s = "";
+string t = "";
+Solution solution = new Solution();
+bool isSubsequence = solution.IsSubsequence(s, t);
+Console.WriteLine(isSubsequence);
+public class Solution {
+    public bool IsSubsequence(string s, string t)
+    {
+        int count = 0;
+        if (s.Length == 0) return true;
+        if (t.Length == 0) return false;
+
+        
+        foreach (var t1 in t)
+        {
+            if (s[count] == t1)
+            {
+                count++;
+                if (count == s.Length)
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+}
