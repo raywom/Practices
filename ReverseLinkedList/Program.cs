eae0bbd2330d8c07990afd7c8fae595f1fa24d01
@raywom <40425058+raywom@users.noreply.github.com>
2022-08-26 09:46:11 +0200

    reverse and merge lists

---
+++ ReverseLinkedList/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+ListNode listNode = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
+Console.WriteLine("Hello, World!");
+Solution solution = new Solution();
+solution.ReverseList(listNode);
+public class Solution {
+    public ListNode ReverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null)
+        {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;        
+    }
+}
+
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
