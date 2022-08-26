ec1dec610d287df0e5e2fa4604c99e57edabd82a
@raywom <40425058+raywom@users.noreply.github.com>
2022-08-27 01:50:48 +0200

    add middleofthelinkedlist

---
+++ MiddleOfTheLinkedList/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+ListNode listNode = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
+Solution solution = new Solution();
+var flex = solution.MiddleNode(listNode);
+Console.WriteLine("hello world");
+public class Solution
+{
+    public ListNode MiddleNode(ListNode head)
+    {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null)
+        {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+}
+
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
